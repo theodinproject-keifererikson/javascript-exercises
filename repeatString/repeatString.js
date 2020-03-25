7d4f68af17ffb52bcf47c46854af5bfc67743449
Keifer Erikson <keiferkode@gmail.com>
2020-03-25 00:01:14 -0600

    Completed helloWorld and repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, count) {
+  let string = "";
+  
+  if (count < 0) {
+    string = "ERROR";
+  }
 
+  for (i = count; i > 0; i--) {
+    string += str;
+  }
+  return string;
 }
 
 module.exports = repeatString
