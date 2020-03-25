e76a2273177639ade392868584ec46c3720b7108
Keifer Erikson <keiferkode@gmail.com>
2020-03-25 11:37:57 -0600

    Completed removeFromArray exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...remove) {
+  let array = [];
 
+  arr.forEach((match) => {
+    if (!remove.includes(match)) {
+      array.push(match);
+    }    
+  });
+  return array;
 }
 
 module.exports = removeFromArray
