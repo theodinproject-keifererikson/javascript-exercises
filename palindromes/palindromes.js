e3fdd33f5673a30b98e3bd817cbd73b5796e82dc
Keifer Erikson <keiferkode@gmail.com>
2020-04-18 21:47:10 -0600

    Completed the palindrome exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function() {
+const palindromes = function(phrase) {
+  const cleanPhrase = phrase.toLowerCase().replace(/[^a-zA-Z]/g, '');  
+  const reversedPhrase = cleanPhrase.split('').reverse().toString().replace(/,/g, '');;
 
+  if (cleanPhrase === reversedPhrase) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = palindromes
