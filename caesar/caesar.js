7c36a59736de7e63e23be5d36cdcab9c3d889a3b
Keifer Erikson <keiferkode@gmail.com>
2020-07-13 12:45:04 -0600

    completed fibonacci in record time

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,30 +1,56 @@
-function caesar(str, shift) {
-  const strArr = str.split('');
-  let charArr = [];
-  let newString = '';
-  
-  for (i = 0; i < str.length; i++) {
-    let charCode = strArr[i].charCodeAt();
-    if (charCode < 65 || (charCode < 97 && charCode > 90) || charCode > 122){
-      if (charCode == 32 || charCode == 33 || charCode == 44) {
-        charArr.push(charCode);
-        // console.log(charArr);
-        continue;
-      }
-      continue;
-    } else {
-      charArr.push(charCode + shift);
-      // console.log(charArr);
-    }
+const caesar = function(string, shift) {
+  return string
+    .split("")
+    .map(char => shiftChar(char, shift))
+    .join("");
+};
+
+const codeSet = code => (code < 97 ? 65 : 97);
+
+const mod = (n, m) => (n % m + m) % m;
+
+const shiftChar = (char, shift) => {
+  const code = char.charCodeAt();
+
+  if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+    return String.fromCharCode(
+      mod(code + shift - codeSet(code), 26) + codeSet(code)
+    );
   }
+  return char;
+};
 
-  charArr.forEach((e) => {
-    newString += String.fromCharCode(e);
-  });
-  // console.log(newString);
-  return newString;
-}
+module.exports = caesar;
 
-// caesar('This! is, a test zzz', 1);
 
-module.exports = caesar
+
+// function caesar(str, shift) {
+//   const strArr = str.split('');
+//   let charArr = [];
+//   let cipher = '';
+  
+//   for (i = 0; i < str.length; i++) {
+//     let charCode = strArr[i].charCodeAt();
+//     if (charCode < 65 || (charCode < 97 && charCode > 90) || charCode > 122){
+//       if (charCode == 32 || charCode == 33 || charCode == 44) {
+//         charArr.push(charCode);
+//         console.log(charArr);
+//         continue;
+//       }
+//       continue;
+//     } else {
+//       charArr.push(charCode + shift);
+//       console.log(charArr);
+//     }
+//   }
+
+//   charArr.forEach((e) => {
+//     cipher += String.fromCharCode(e);
+//   });
+//   console.log(cipher);
+//   return cipher;
+// }
+
+// caesar('This! is, a test zzz', 2);
+
+// // module.exports = caesar
--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+  let fibArr = [1, 1];
 
+  if (count < 0)  {return 'OOPS';}
+  if (count == 0) {return 0;}
+   
+  for (i = 0; i < count - 2; i++) {
+    fibArr.push(fibArr[fibArr.length - 1] + fibArr[fibArr.length - 2]);
+  }
+  
+  return fibArr[fibArr.length - 1];
 }
 
 module.exports = fibonacci
