f42e36413b49356cf82c1e291211930dc0d0f74e
Keifer Erikson <keiferkode@gmail.com>
2020-07-13 12:51:30 -0600

    Completed getTheTitles first try in 2 minutes, maybe did it wrong

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+  bookArr = [];
 
+  bookArr.push(books[0].title)
+  bookArr.push(books[1].title)
+
+  return bookArr;
 }
 
 module.exports = getTheTitles;
