a439bc78ec1c8323da8317330a2233f6614c280c
Keifer Erikson <keiferkode@gmail.com>
2020-07-13 12:55:14 -0600

    discovered there was a better way to complete getTheTitles

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,10 +1,16 @@
-const getTheTitles = function(books) {
-  bookArr = [];
-
-  bookArr.push(books[0].title)
-  bookArr.push(books[1].title)
-
-  return bookArr;
+const getTheTitles = function(arr) {
+  return arr.map(book => book.title);
 }
+
+
+
+// const getTheTitles = function(books) {
+//   bookArr = [];
+
+//   bookArr.push(books[0].title)
+//   bookArr.push(books[1].title)
+
+//   return bookArr;
+// }
 
 module.exports = getTheTitles;
