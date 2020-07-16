c9f4240c5ece8cd6f32dff0e637f72757f91ba50
Keifer Erikson <keiferkode@gmail.com>
2020-07-15 23:20:21 -0600

    switching to laptop

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-let findTheOldest = function() {
+const people = [
+  {
+    name: 'Carly',
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: 'Ray',
+    yearOfBirth: 1962,
+    yearOfDeath: 2011
+  },
+  {
+    name: 'Jane',
+    yearOfBirth: 1912,
+    yearOfDeath: 1941
+  },
+]
+
+
+
+console.log('This is now reloading');
+
+let findTheOldest = function() { 
 
 }
 
-module.exports = findTheOldest
+// module.exports = findTheOldest
